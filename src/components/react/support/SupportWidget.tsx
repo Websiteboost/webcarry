de2386a1c182e1a5ea8a142f5294d4211e5f3e24
AzanoRivers <job.devcode@gmail.com>
2026-06-23 00:30:36 -0500

    update astro7, update packages, install suportum-chat witch apikey

---
+++ src/components/react/support/SupportWidget.tsx
@@ -0,0 +1,11 @@
+import { SuportumChat } from 'suportum-chat'
+
+export default function SupportWidget() {
+  return (
+    <SuportumChat
+      apiUrl="https://chat.azanolabs.com"
+      apiKey="sproj_31ade761eff7d54d8da9c21f2da1edcb"
+      locale="en"
+    />
+  )
+}
